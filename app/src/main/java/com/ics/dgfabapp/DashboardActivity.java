0e8467dddb8a43322ddc5eb5416b6d06b384002e
NothingbutPro <paragsharma611@gmail.com>
2019-04-29 15:20:14 +0530

    by pro

--- app/src/main/java/com/ics/dgfabapp/DashboardActivity.java
+++ app/src/main/java/com/ics/dgfabapp/DashboardActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -36,7 +37,7 @@ import java.util.List;
 import java.util.Random;
 
 
-public class DashboardActivity extends AppCompatActivity  {
+public class DashboardActivity extends AppCompatActivity  implements   NavigationView.OnNavigationItemSelectedListener {
     RecyclerView recycler_view;
     MyListData[] myListData;
     View view;
@@ -82,7 +83,7 @@ public class DashboardActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_dashboard_navigation);
         sessionManager = new SessionManager(DashboardActivity.this);
 //******************************************************************************************
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -90,7 +91,14 @@ public class DashboardActivity extends AppCompatActivity  {
         /* start 2 months ago from now */
         Calendar startDate = Calendar.getInstance();
         startDate.add(Calendar.MONTH, -2);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layoutxyz);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         /* end after 2 months from now */
         Calendar endDate = Calendar.getInstance();
         endDate.add(Calendar.MONTH, 2);
@@ -257,5 +265,33 @@ public class DashboardActivity extends AppCompatActivity  {
      //   mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_addprod) {
+            Intent intent = new Intent(DashboardActivity.this, AddProduct.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
