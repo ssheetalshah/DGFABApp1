3351222236f09a3b30dd4def4defe296755d42b7
Raghvendra Sahu <raghvendra.19934@gmaial.com>
2019-05-07 12:54:54 +0530

    by rrrrr

--- app/src/main/java/com/ics/dgfabapp/Voucher_Activity.java
+++ app/src/main/java/com/ics/dgfabapp/Voucher_Activity.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class Voucher_Activity extends AppCompatActivity {
-    TextView reciept_tv;
+    LinearLayout reciept_tv;
     Toolbar toolbar_voucher;
 
     @Override
