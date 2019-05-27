e718a90764171f9a009350358e5a000f4b7dd686
ssheetalshah <sshahsheetal94@gmail.com>
2019-05-27 13:31:42 +0530

    hello world

--- app/src/main/java/com/ics/dgfabapp/Check_out.java
+++ app/src/main/java/com/ics/dgfabapp/Check_out.java
@@ -3,19 +3,29 @@ package com.ics.dgfabapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class Check_out extends AppCompatActivity {
-    Button place_or,place_add_to_cart;
-
+    Button place_or, place_add_to_cart;
+    Toolbar toolbar_checkout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
+
+        toolbar_checkout = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_checkout);
+        toolbar_checkout.setNavigationIcon(R.drawable.home);
+        toolbar_checkout.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         place_or = findViewById(R.id.place_or);
         place_add_to_cart = findViewById(R.id.place_add_to_cart);
@@ -24,7 +34,7 @@ public class Check_out extends AppCompatActivity {
         place_or.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Check_out.this , Order_Placed.class);
+                Intent intent = new Intent(Check_out.this, Order_Placed.class);
                 startActivity(intent);
             }
         });
@@ -35,9 +45,14 @@ public class Check_out extends AppCompatActivity {
 
                 Toast.makeText(Check_out.this, "Add To Cart Successfully", Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(Check_out.this , NavigationActivity.class);
+                Intent intent = new Intent(Check_out.this, NavigationActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/ics/dgfabapp/Order_Placed.java
+++ app/src/main/java/com/ics/dgfabapp/Order_Placed.java
@@ -3,23 +3,49 @@ package com.ics.dgfabapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Order_Placed extends AppCompatActivity {
     Button donbtn;
+    ImageView success;
+    Toolbar toolbar_success;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order__placed);
         donbtn = findViewById(R.id.donbtn);
+
+        toolbar_success = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_success);
+        toolbar_success.setNavigationIcon(R.drawable.home);
+        toolbar_success.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        //***********animation Bounce
+        final Animation bounce = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+        success = (ImageView) findViewById(R.id.success);
+        success.startAnimation(bounce);
+
         donbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Order_Placed.this , NavigationActivity.class);
+                Intent intent = new Intent(Order_Placed.this, NavigationActivity.class);
                 startActivity(intent);
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
