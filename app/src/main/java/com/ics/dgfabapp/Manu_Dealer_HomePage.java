60497674494368aa6e63904f01ece2c8d911623c
Raghvendra Sahu <raghvendra.19934@gmail.com>
2019-06-08 18:40:23 +0530

    by rrrr

---
+++ app/src/main/java/com/ics/dgfabapp/Manu_Dealer_HomePage.java
@@ -0,0 +1,13 @@
+package com.ics.dgfabapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Manu_Dealer_HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manu__dealer__home_page);
+    }
+}
