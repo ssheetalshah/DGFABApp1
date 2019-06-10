d25824ee5035cf545c833dfe7d4982af2aef6ad6
ssheetalshah <sshahsheetal94@gmail.com>
2019-06-10 13:21:08 +0530

    hello world

--- app/src/main/java/com/ics/dgfabapp/LinkProfile.java
+++ app/src/main/java/com/ics/dgfabapp/LinkProfile.java
@@ -2,9 +2,13 @@ package com.ics.dgfabapp;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +17,9 @@ public class LinkProfile extends AppCompatActivity {
         TextView mycons;
         TextView interest;
         FloatingActionButton fabeditpro;
+        Toolbar toolbar_prff;
+        SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,7 @@ public class LinkProfile extends AppCompatActivity {
         edit_pro = findViewById(R.id.edit_pro);
         interest = findViewById(R.id.interest);
         mycons = findViewById(R.id.mycons);
+        toolbar_prff = findViewById(R.id.toolbar_prff);
         edit_pro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +58,26 @@ public class LinkProfile extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        toolbar_prff.setNavigationIcon(getResources().getDrawable(R.drawable.home));
+        toolbar_prff.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
+        searchView = (SearchView)findViewById(R.id.searchh);
+        ImageView searchIcon = searchView.findViewById(android.support.v7.appcompat.R.id.search_button);
+        searchIcon.setImageDrawable(ContextCompat.getDrawable(LinkProfile.this,R.drawable.search));
+        EditText searchEditText = (EditText)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(getResources().getColor(R.color.white));
+        searchEditText.setHintTextColor(getResources().getColor(R.color.white));
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
